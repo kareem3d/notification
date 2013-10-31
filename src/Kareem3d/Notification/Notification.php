c06113eeabac4074bc6fc3e45b457d0ae384861f
Kareem Mohamed <kareem3d.a@gmail.com>
2013-10-31 13:02:05 +0200

    V1.4

--- src/Kareem3d/Notification/Notification.php
+++ src/Kareem3d/Notification/Notification.php
@@ -9,28 +9,7 @@ class Notification extends Model {
      *
      * @var string
      */
-    protected $table = 'notifications';
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = array();
-
-    /**
-     * The attributes that can't be mass assigned
-     *
-     * @var array
-     */
-    protected $guarded = array('id');
-
-    /**
-     * Whether or not to softDelete
-     *
-     * @var bool
-     */
-    protected $softDelete = false;
+    protected $table = 'ka_notifications';
 
     /**
      * Validations rules
@@ -39,20 +18,4 @@ class Notification extends Model {
      */
     protected $rules = array(
     );
-
-    /**
-     * For factoryMuff package to be able to fill attributes.
-     *
-     * @var array
-     */
-    public static $factory = array(
-    );
-
-    /**
-     * @return string
-     */
-    public function getBody()
-    {
-        return $this->body;
-    }
 }
